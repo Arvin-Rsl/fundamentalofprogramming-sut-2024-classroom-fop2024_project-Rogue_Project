9d4d93580a8596fd7e3a6ef8ccb825f2cef707c8
Arvin-Rsl <arvinrasulzadeh@gmail.com>
2025-02-02 18:39:06 +0330

    Rogue
    Forgot to commit HERE...

---
+++ TOTAL.c
@@ -0,0 +1,18 @@
+// Arvin Rasulzadeh  --  4 0 3 1 1 0 4 2 2  -- FOP Project Phase 1
+
+#include "TOTAL.h"
+
+int main(){
+    setlocale(LC_ALL, "");
+    initscr();
+    noecho();
+    cbreak();
+    start_color(); 
+    bkgd(COLOR_BLACK);
+    while (1){
+        entrance_menu();
+        refresh();
+    }
+    endwin();
+    return 0;
+}
